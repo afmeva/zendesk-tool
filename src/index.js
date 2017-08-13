93ddf828350464437b431075abe916e04bd9183b
afmeva <afmeva@gmail.com>
2017-08-13 16:28:07 -0500

    WIP adding provider component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+// redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import Routes from './routes';
 
 import './index.css';
 
+// const store = createStore(reducer)
+const store = createStore(() => {})
+
 ReactDOM.render((
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'))
