ff8e99b17d14f50c8ab8c58c5bdcf33ea86a4b57
afmeva <afmeva@gmail.com>
2017-08-14 20:03:22 -0500

    fixing a bug that was preventing continuos writing on the form

--- src/form-ticket/index.js
+++ src/form-ticket/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form'
 
 const validate = values => {
+  console.log('validating');
   const errors = {}
   if (values.subject === undefined || values.subject === '') {
     errors.subject = 'Required'
@@ -14,21 +15,24 @@ const validate = values => {
   return errors
 }
 
-const renderField = (Tag, { input, label, type, className, meta: { touched, error } }) => (
-  <div className='form-group'>
-    <label>{label}</label>
-    <Tag {...input} placeholder={label} type={type} className='form-control'/>
-    {touched && error ? error: ''}
-  </div>
-)
+const renderField = ({ tag, input, label, type, className, meta: { touched, error } }) => {
+  let Component = tag
+  return (
+    <div className='form-group'>
+      <label>{label}</label>
+      <Component {...input} placeholder={label} type={type} className='form-control' />
+      {touched && error ? error: ''}
+    </div>
+  )
+}
 
 let FormTicket = (props) => {
   const {handleSubmit} = props
 
   return (
     <form className='create-ticket__form' onSubmit={handleSubmit}>
-      <Field label="Ticket subject:" id='subject' name='subject' component={renderField.bind(null, 'input')} type='text' />
-      <Field label="Issue description:" name='description' component={renderField.bind(null, 'textarea')} type='text' />
+      <Field tag="input" label="Ticket subject:" id='subject' name='subject' component={renderField} type='text' />
+      <Field tag="textarea" label="Issue description:" name='description' component={renderField} />
       <button type='submit' className="btn btn-default">Submit</button>
     </form>
   );
